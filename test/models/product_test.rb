2e9d43876ae4249d3a18483613bec323214377a4
jrmossad <jrmossad@gmail.com>
2020-04-25 19:39:46 +0200

    add order tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -13,11 +13,9 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do
-    product = Product.new(title:
-    "My Book Title",
+    product = Product.new(title: "My Book Title",
     description: "yyy",
-    image_url:
-    "zzz.jpg")
+    image_url: "zzz.jpg")
     product.price = -1
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"],
